87c352a911817f2ace7e9b2f4a7cbab9281b57d0
Lasse Fröhner <lasse@starcopter.com>
2025-06-05 13:59:52 +0200

    Document device client

--- src/cyphal_device_library/mmb/client.py
+++ src/cyphal_device_library/mmb/client.py
@@ -8,7 +8,7 @@ from ..device_client import DeviceClient
 
 class MMBClient(DeviceClient):
     DEFAULT_NAME = "com.starcopter.test.aeric.mmb"
-    DEVICE_NAME = "com.starcopter.aeric.mmb"
+    DEFAULT_DUT_NAME = "com.starcopter.aeric.mmb"
 
     def system_status_subscription(self) -> AbstractContextManager[Subscriber[starcopter.aeric.mmb.SystemStatus_0_1]]:
         assert self.registry["uavcan.pub.system_status.type"].value == "starcopter.aeric.mmb.SystemStatus.0.1"
