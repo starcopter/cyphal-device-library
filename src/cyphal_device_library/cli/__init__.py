3e8650ccc7a390f3f2618c8b58c27235780dd81c
Lasse Fröhner <lasse@starcopter.com>
2025-06-11 09:05:39 +0200

    Fix import errors

--- src/cyphal_device_library/cli/__init__.py
+++ src/cyphal_device_library/cli/__init__.py
@@ -2,13 +2,19 @@ from typing import Annotated
 
 import typer
 
-from . import discover, dsdl, update
+from . import dsdl
 from ._util import configure_logging, set_default_usbtingo_env_vars
 
 app = typer.Typer()
 app.add_typer(dsdl.app)
-app.add_typer(update.app)
-app.add_typer(discover.app)
+
+try:
+    from . import discover, update
+
+    app.add_typer(update.app)
+    app.add_typer(discover.app)
+except ImportError:
+    app.info.epilog = "Run `cyphal install` to make more commands available."
 
 
 @app.callback()
