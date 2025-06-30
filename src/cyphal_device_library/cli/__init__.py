acef57fad953efd78bc9401a2f4803238a509e7b
Sven Gesper <sven@starcopter.com>
2025-06-30 14:28:31 +0200

    misc: logging of no buffer space available silenced. cyphal diagnostic record with custom argument for verbosity [-d]

--- src/cyphal_device_library/cli/__init__.py
+++ src/cyphal_device_library/cli/__init__.py
@@ -3,8 +3,11 @@ from typing import Annotated
 import typer
 from dotenv import load_dotenv
 
+import logging
+
 from . import dsdl
 from ._util import configure_logging, set_default_usbtingo_env_vars
+from ..client import Client
 
 app = typer.Typer()
 app.add_typer(dsdl.app)
@@ -21,6 +24,15 @@ except ImportError:
 @app.callback()
 def main(
     verbosity: Annotated[int, typer.Option("--verbose", "-v", count=True)] = 0,
+    diagnostic_record_verbosity: Annotated[
+        int,
+        typer.Option(
+            "--diagnostic",
+            "-d",
+            help="Set the logging level for Cyphal DiagnosticRecords, e.g. in discovery",
+            count=True,
+        ),
+    ] = 3,
     reload: Annotated[bool, typer.Option("--reload", "-r", help="Reload environment from .env file")] = False,
     pnp: Annotated[
         bool, typer.Option("--pnp-server", "-p", help="Launch PnP server for dynamic node ID allocation.")
@@ -30,3 +42,32 @@ def main(
         load_dotenv(override=True)
     configure_logging(verbosity)
     set_default_usbtingo_env_vars()
+
+    diagnost_record_logger = logging.getLogger("uavcan.diagnostic.record")
+    diagnost_record_logger.setLevel(Client._UAVCAN_SEVERITY_TO_PYTHON[7 - diagnostic_record_verbosity])
+    level_name = logging.getLevelName(diagnost_record_logger.level)
+    print(f"[-d|--diagnostic] Setting diagnostic record verbosity to {level_name}")
+
+    # Suppress heartbeat publisher errors (e.g. no cyphal node receive CAN messages)
+    class HeartbeatExceptionFilter(logging.Filter):
+        def filter(self, record):
+            if "publisher task exception:" in record.getMessage():
+                if "[Errno 105] No buffer space available" in record.getMessage():
+                    # print("[Errno 105] Heartbeat Publish: no buffer space available")
+                    return False
+            return True
+
+    heartbeat_publish_logger = logging.getLogger("pycyphal.application.heartbeat_publisher")
+    heartbeat_publish_logger.addFilter(HeartbeatExceptionFilter())
+
+    # Suppress publisher port errors (e.g. no cyphal node receive CAN messages)
+    class PresentationPublishFilter(logging.Filter):
+        def filter(self, record):
+            if "deferred publication has failed" in record.getMessage():
+                if "[Errno 105] No buffer space available" in record.getMessage():
+                    # print("[Errno 105] Presentation Layer Publish: no buffer space available")
+                    return False
+            return True
+
+    presentation_publisher_logger = logging.getLogger("pycyphal.presentation._port._publisher")
+    presentation_publisher_logger.addFilter(PresentationPublishFilter())
